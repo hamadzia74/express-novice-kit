11cc70d8cf4b813a311b798cd63efd09ae197f98
Hamad Zia <hamadzia74@gmail.com>
2025-08-17 14:52:23 +0500

    Stored Product Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,7 +19,14 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   console.log(req.body)
   // products.push({ title: req.body.title })
-  const product = new Product(req.body.title) // create a new object based on this class blueprint and that is what classes are in the end, they are blueprints.
+  //   const product = new Product(req.body.title) // create a new object based on this class blueprint and that is what classes are in the end, they are blueprints.
+
+  const title = req.body.title
+  const imageUrl = req.body.imageUrl
+  const price = req.body.price
+  const description = req.body.description
+  const product = new Product(title, imageUrl, price, description)
+
   product.save()
   res.redirect('/') // redirect to the root URL
 }
