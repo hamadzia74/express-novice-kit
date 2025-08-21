9863c737b3607d6fed604551109f30fef29be2f9
Hamad Zia <hamadzia74@gmail.com>
2025-08-21 16:09:16 +0500

    Linked to the Edit Page

--- controllers/admin.js
+++ controllers/admin.js
@@ -51,6 +51,10 @@ exports.getEditProduct = (req, res, next) => {
   })
 }
 
+exports.postEditProduct = (req, res, next) => {
+
+}
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render('admin/products', {
