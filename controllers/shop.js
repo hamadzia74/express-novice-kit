ad5b2a0debb2f6c0e97b7eaf11b9b96dd65057f9
Hamad Zia <hamadzia74@gmail.com>
2025-08-22 16:36:57 +0500

    Deleted Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -62,7 +62,7 @@ exports.getCart = (req, res, next) => {
       res.render('shop/cart', {
         pageTitle: 'Your Cart',
         path: '/cart',
-        products: cartProducts
+        products: cartProducts,
       })
     })
   })
@@ -74,6 +74,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price)
   })
   res.redirect('/cart')
+}
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price)
+    res.redirect('/cart')
+  })
 }
 
 exports.getOrders = (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,7 @@ const express = require('express')
 
 const rootDir = require('../util/path')
 const adminData = require('./admin')
-const { getProducts, getIndex, getCart, getCheckout, getOrders, getProduct, postCart } = require('../controllers/shop')
+const { getProducts, getIndex, getCart, getCheckout, getOrders, getProduct, postCart, postCartDeleteProduct } = require('../controllers/shop')
 
 const router = express.Router()
 
@@ -33,6 +33,9 @@ router.get('/products/:productId', getProduct)
 
 router.get('/cart', getCart)
 router.post('/cart', postCart)
+
+router.post('/cart-delete-item', postCartDeleteProduct)
+
 router.get('/orders', getOrders)
 router.get('/checkout', getCheckout)
 
