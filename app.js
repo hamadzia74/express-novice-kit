b977376c46e8538928f3e659b46d9cadb4e1a536
Hamad Zia <hamadzia74@gmail.com>
2025-08-29 12:41:16 +0500

    Basic SQL & Creating a Table

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.set('views', 'views') // set the views directory where the templates are loc
 //   next();
 // });
 
-db.execute('SELECT * FROM products');
+db.execute('SELECT * FROM products').then().catch()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public'))) // serve static files from the public directory
